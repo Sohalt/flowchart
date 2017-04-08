d5318ae623ee27fef6c82739a0a1961177f2d5e5
sohalt <sohalt@sohalt.net>
2017-04-08 22:48:49 +0200

    abstract Component protocol

--- src/cljs/flowchart/core.cljs
+++ src/cljs/flowchart/core.cljs
@@ -3,24 +3,38 @@
             [goog.string :as gstring]
             [goog.string.format]))
 
+;; -------------------------
+;; State
+
+(defonce elems (atom []))
 
 ;; -------------------------
 ;; Views
 
-(defn stmt [x y text]
-  (let [w 120
-        h 40]
-    [:g {:transform (gstring/format "translate(%d,%d)" x y)}
-     [:rect {:width w :height h :style {:fill "none" :stroke "black"}}]
-     [:text {:x 5 :y (* h .6)}text]]))
+(defprotocol Component
+  (render [_]))
 
-(defn branch [x y text]
-  (let [l 80]
-    [:g {:transform (gstring/format "translate(%d,%d)" x y)}
-     [:rect { :width l :height l
-                :style {:fill "none" :stroke "blue"}
-                :transform "rotate(45)"}]
-     [:text text]]))
+(defrecord Stmt [x y text]
+  Component
+  (render [_]
+    (let [w 120
+          h 40]
+      (.log js/console "foo")
+      [:g {:transform (gstring/format "translate(%d,%d)" x y)
+           :on-click #(js/alert "foo")}
+       [:rect {:width w :height h :style {:fill "none" :stroke "black"}}]
+       [:text {:x 5 :y (* h .6)}text]])))
+
+(defrecord Branch [x y text]
+  Component
+  (render [_]
+    (let [l 80]
+      [:g {:transform (gstring/format "translate(%d,%d)" x y)}
+       [:rect {:width l :height l
+               :style {:fill "none" :stroke "blue"}
+               :transform "rotate(45)"}]
+       [:polygon {:points ""}]
+       [:text text]])))
 
 (defn svg-component
   [& elems]
@@ -34,10 +48,7 @@
 (defn svg-page []
   [:div [:h2 "Some SVG"]
    [svg-component
-    [:circle {:cx 60 :cy 50 :r 60 :style {:fill "black"}}]
-    [:circle {:cx 60 :cy 10 :r 60 :style {:fill "red"}}]
-    (map #(stmt 120 % (str "abc " %)) (range 20 200 50))
-    [branch 300 100 "bar"]]])
+    (map render @elems)]])
 
 ;; -------------------------
 ;; Initialize app
