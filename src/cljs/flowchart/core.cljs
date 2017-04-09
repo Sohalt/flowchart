da93768f57c93450b3f173e308880c954694f978
sohalt <sohalt@sohalt.net>
2017-04-09 16:33:19 +0200

    proper arrow head, only draw arrows when starting from stmt

--- src/cljs/flowchart/core.cljs
+++ src/cljs/flowchart/core.cljs
@@ -35,16 +35,16 @@
       (fn []
         [:g {:transform (gstring/format "translate(%d,%d)" (- @x w') (- @y h'))
              :on-mouse-down #(case (.-button %)
-                               0 (reset! left-pressed? true)
+                               0 (do (reset! left-pressed? true) (swap! mouse-state assoc-in [:left :start-elem] :stmt))
                                1 (reset! middle-pressed? true))
              :on-mouse-move (fn [e]
                               (when @middle-pressed?
                                 (reset! x (.-clientX e))
                                 (reset! y (.-clientY e))))
              :on-mouse-up #(do (case (.-button %)
-                                 0 (reset! left-pressed? false)
+                                 0 (do (reset! left-pressed? false) (swap! mouse-state assoc-in [:left :end-elem] :stmt))
                                  1 (reset! middle-pressed? false)))}
-         [:rect {:width w :height h :style {:fill "blue" :stroke "black"}}]
+         [:rect {:width w :height h :style {:fill "blue"}}]
          [:text {:x 5 :y (* h .6)}text]]))))
 
 (defrecord Branch [x y text]
@@ -59,7 +59,7 @@
        [:text text]])))
 
 (defn arrow [from to]
-  (svg/line from to {:style {:stroke "black"}}))
+  [:g (svg/line-decorated from to nil (svg/arrow-head 10 (/ Math/PI 4) true))])
 
 (defn- button-down! [button x y]
   (swap! mouse-state update button merge {:pressed? true :dragstart [x y]}))
@@ -78,7 +78,7 @@
   (swap! mouse-state update-drag x y))
 
 (defn- button-up! [button]
-  (swap! mouse-state update button merge {:pressed? false :delta [0 0]}))
+  (swap! mouse-state update button merge {:pressed? false :delta [0 0] :start-elem nil}))
 
 (defn svg-component
   [& body]
@@ -86,9 +86,10 @@
    {:width (.-innerWidth js/window)
     :height (.-innerHeight js/window)
     :id "canvas"
-    :style {:outline "2px solid black"
-            :background-color "#fff"
-            :display "block"}
+    :style {:background-color "#fff"
+            :display "block"
+            :stroke "black"}
+    :on-click (fn [e] (when (= 0 (.-button e)) (swap! elems conj (->Stmt (.-clientX e) (.-clientY e) "foo"))))
     :on-mouse-down (fn [e]
                      (let [x (.-clientX e)
                            y (.-clientY e)]
@@ -115,13 +116,13 @@
   [:div
    [svg-component
     [:text {:x 50 :y 50} (with-out-str (pprint @mouse-state))]
+    (for [elem @elems]
+        [render elem])
     (let [s @mouse-state
           from (get-in s [:left :dragstart])
           to (map + from (get-in s [:left :delta]))]
-      (when (get-in s [:left :pressed?])
-        [arrow from to]))
-    #_(for [elem @elems]
-        [render elem])]])
+      (when (and (get-in s [:left :pressed?]) (= (get-in s [:left :start-elem]) :stmt))
+        [arrow from to]))]])
 
 ;; -------------------------
 ;; Initialize app
