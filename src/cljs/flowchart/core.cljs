774a730d83c81f87ab6f9d34f7d341e6965fafde
sohalt <sohalt@sohalt.net>
2017-04-14 12:06:16 +0200

    cleaner state management, multimethod instead of protocols for rendering

--- src/cljs/flowchart/core.cljs
+++ src/cljs/flowchart/core.cljs
@@ -8,12 +8,13 @@
 ;; -------------------------
 ;; State
 
-(defonce elems (atom []))
+(defonce elems (atom {}))
 
 (defonce mouse-state (atom {:left {:pressed? false :dragstart [0 0] :delta [0 0]}
                             :middle {:pressed? false :dragstart [0 0] :delta [0 0]}
                             :right {:pressed? false :dragstart [0 0] :delta [0 0]}}))
 
+(defonce drag (atom nil))
 
 ;; -------------------------
 ;; Views
@@ -21,45 +22,42 @@
 (defn arrow [from to]
   [:g {:key (gensym)} (svg/line-decorated from to nil (svg/arrow-head 10 (/ Math/PI 4) true))])
 
-(defprotocol Component
-  (render [_]))
+(defn dragged? [id]
+  (reagent/track #(= @drag id)))
 
-(defrecord Stmt [x y text]
-  Component
-  (render [_]
-    (let [w 120
-          h 40
-          outlinks (atom [])
-          pos (atom [x y])
-          drag? (atom false)]
-      (fn []
-        (vec
-         (concat
-          [:g
-           [:g {:transform (apply gstring/format "translate(%d,%d)"
-                                  (map + @pos
-                                       (if @drag?
-                                         (get-in @mouse-state [:middle :delta])
-                                         [0 0])))
-                :on-mouse-down #(case (.-button %)
-                                  0 (swap! mouse-state assoc-in [:left :start-elem] [:stmt outlinks])
-                                  1 (reset! drag? true))
-                :on-mouse-up #(case (.-button %)
-                                0 (let [s @mouse-state
-                                        left-pressed? (get-in s [:left :pressed?])
-                                        [label links] (get-in s [:left :start-elem])]
-                                    (when (and left-pressed? (= label :stmt))
-                                      (swap! links conj pos)))
-                                1 (do
-                                    (swap! pos (partial map + (get-in @mouse-state [:middle :delta])))
-                                    (reset! drag? false)))
-                :key (gensym)}
-            [:rect {:width w :height h :style {:fill "blue"}}]
-            [:text {:x 5 :y (* h .6)} text]]]
-          (mapv (fn [dest] (arrow @pos @dest))
-                @outlinks)))))))
+(defn dragged! [id]
+  (reset! drag id))
 
-(defrecord Branch [x y text]
+(defn internal-pos [id]
+  (reagent/track #(get-in @elems [id :pos])))
+
+(defn actual-pos [id]
+  (reagent/track #(map + @(internal-pos id) (if @(dragged? id)
+                                              (get-in @mouse-state [:middle :delta])
+                                              [0 0]))))
+
+(defn stmt [x y text]
+  (let [id (gensym)]
+    {:id id
+     :type :stmt
+     :text text
+     :pos [x y]}))
+
+(defmulti render (fn [elem] (:type elem)))
+
+(defmethod render :stmt [{:keys [id text]}]
+  (let [w 120
+        h 40]
+    [:g {:transform (apply gstring/format "translate(%d,%d)"
+                           @(actual-pos id))
+         :on-mouse-down #(case (.-button %)
+                           1 (dragged! id))
+         :on-mouse-up #(case (.-button %)
+                         1 (swap! elems assoc-in [id :pos] @(actual-pos id)))}
+     [:rect {:width w :height h :style {:fill "blue"}}]
+     [:text {:x 5 :y (* h .6)} text]]))
+
+#_(defrecord Branch [x y text]
   Component
   (render [_]
     (let [l 80]
@@ -77,7 +75,6 @@
   (let [[x y] (get-in s [k :dragstart])]
     (assoc-in s [k :delta] [(- x' x) (- y' y)])))
 
-
 (defn- update-drag [state x y]
   (let [pressed-keys (keys (filter (fn [[button {:keys [pressed?]}]] pressed?)
                                    state))]
@@ -87,7 +84,8 @@
   (swap! mouse-state update-drag x y))
 
 (defn- button-up! [button]
-  (swap! mouse-state update button merge {:pressed? false :delta [0 0] :start-elem nil}))
+  (do (swap! mouse-state update button merge {:pressed? false :delta [0 0] :start-elem nil})
+      (reset! drag nil)))
 
 (defn svg-component
   [& body]
@@ -98,7 +96,9 @@
     :style {:background-color "#fff"
             :display "block"
             :stroke "black"}
-    :on-click (fn [e] (when (= 0 (.-button e)) (swap! elems conj (->Stmt (.-clientX e) (.-clientY e) "foo"))))
+    :on-click (fn [e] (when (= 0 (.-button e))
+                        (let [{:keys [id] :as elem} (stmt (.-clientX e) (.-clientY e) "foo")]
+                          (swap! elems assoc id elem))))
     :on-mouse-down (fn [e]
                      (let [x (.-clientX e)
                            y (.-clientY e)]
@@ -123,13 +123,13 @@
   [:div
    [svg-component
     [:text {:x 50 :y 50} (with-out-str (pprint @mouse-state))]
-    (let [s @mouse-state
+    #_(let [s @mouse-state
           from (get-in s [:left :dragstart])
           to (map + from (get-in s [:left :delta]))]
       (when (and (get-in s [:left :pressed?]) (= (get-in s [:left :start-elem 0]) :stmt))
         [arrow from to]))
-    (for [elem @elems]
-        [render elem])]])
+    (for [elem (vals @elems)]
+      [render elem])]])
 
 ;; -------------------------
 ;; Initialize app
